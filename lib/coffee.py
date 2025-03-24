8c3b8fcbe22ee2bed63e7ad39ba4811d08f24c69
David Doan <daviddoan@Davids-MacBook-Pro.local>
2025-03-24 12:45:08 -0600

    solution

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self,size,price):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if size in ["Small","Medium","Large"]:
+            self._size = size
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here’s a tip!")
